dc3f9bfb64024913b3915ea6c60cd0d3ae97a64b
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-01 17:54:29 +0200

    Задача про часовую ставку

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+
+const stavkaHour = 80;
+let workTime = dlsworkTime = 40;
+let days = 11-2;
+let payDay = undefined;
+for (i=0;i<days;i++){
+    dlsworkTime-=5;
+}
+if (dlsworkTime<=0){
+    console.log('Смогу ли я взять работу? ' + true);
+    payDay=workTime*stavkaHour;
+    console.log('Стоимость работ: ' + payDay + '$');
+}
