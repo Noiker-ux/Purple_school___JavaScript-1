32fc42233d6582e0b8d61932a94384764f26e8d1
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 11:07:17 +0200

    Задача Проверка на робота (Вариант 1)

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const deposit = 12000;
-const rate = 0.07;
-const depositLength = 24;
-const priceHouse = 13500;
-const itog = deposit * (1 + rate/12) ** depositLength;
-console.log(`Вася накопил: ${itog}`);
-itog>=priceHouse?
-console.log(`Сможет ли Вася купить дом за ${priceHouse}$: ${true}\nОстаток после покупки дома составляет ${itog-priceHouse}$`)
-:
-console.log(`На данный момент покупка дома невозможна. \n Для покупки дома не хватает ${priceHouse-itog}$`);
-// |
+const answer = prompt('Сколько будет 7 + или - 15?');
+if (answer==-8 || answer==22) {
+    console.log('Успех');
+} else if ('Я не робот' === answer){
+    console.log('Успех');
+} else {
+    console.log('Вы робот');
+}
