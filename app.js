c5339adf911ef294625e7cb1f33cca30ac67e6eb
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 10:24:54 +0200

    Задача про депозит

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-
-const stavkaHour = 80;
-let workTime = dlsworkTime = 40;
-let days = 11-2;
-for (i=0;i<days;i++){
-    dlsworkTime-=5;
-}
-if (dlsworkTime<=0){
-    console.log(`Смогу ли я взять работу? ${true}`);
-    console.log(`Стоимость работ: ${workTime*stavkaHour}$`);
+const deposit = 12000;
+const rate = 0.07;
+const depositLength = 24;
+const priceHouse = 13500;
+const itog = deposit * (1 + rate/12) ** depositLength;
+console.log(`Вася накопил: ${itog}`);
+if (itog>=priceHouse){
+    console.log(`Сможет ли Вася купить дом за ${priceHouse}$: ${true}`);
+    console.log(`Остаток после покупки дома составляет ${itog-priceHouse}$`);
+} else {
+    console.log('На данный момент покупка дома невозможна.');
+    console.log(`Для покупки дома не хватает ${priceHouse-itog}$`);
 }
