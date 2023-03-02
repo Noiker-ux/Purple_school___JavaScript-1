5240c1f967e1bb77262b7119d130bb63a9f6751b
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 16:07:37 +0200

    Задача на покупку MacBook'a

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-const funcTrain = (num, power) => {
-    const res =num**power; 
-    return res;
-}
-console.log(funcTrain(2,3));
+const bigCredit = (ageUser, workUser, moneyUser) => {
+    if (Number(ageUser)>24 && workUser===true){
+        moneyUser+=500;
+    };
+    return moneyUser;
+};
+
+const smallCredit = (ageUser, moneyUser) => {
+    if (Number(ageUser)>24){
+        moneyUser+=100;
+    };
+    return moneyUser;
+};
+
+const checkBuy = (moneyUser, priceMac) => {
+    if (Number(moneyUser)>=Number(priceMac)){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const buyMac = (ageUser, moneyUser, workUser, priceMac) => {
+    if (checkBuy(moneyUser,priceMac)){
+        console.log('Можно купить без кредитов');
+        return true;
+    } else if (checkBuy (smallCredit(ageUser, moneyUser), priceMac )) {
+        console.log('Можно купить c маленьким кредитом');
+        return true;
+    } else if (checkBuy (bigCredit(ageUser, workUser, moneyUser),priceMac)) {
+        console.log('Можно купить c большим кредитом');
+        return true;
+    } else {
+        console.log('Невозможно купить');
+        return false;
+    }
+};
+
+console.log(buyMac(32,1400,true,2000));
