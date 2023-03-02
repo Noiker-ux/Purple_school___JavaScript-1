280170082ccb9d76f1500e2d1bc5b49f6a931707
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 11:01:06 +0200

    Тернарные операторы (тренировка)

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const depositLength = 24;
 const priceHouse = 13500;
 const itog = deposit * (1 + rate/12) ** depositLength;
 console.log(`Вася накопил: ${itog}`);
-if (itog>=priceHouse){
-    console.log(`Сможет ли Вася купить дом за ${priceHouse}$: ${true}`);
-    console.log(`Остаток после покупки дома составляет ${itog-priceHouse}$`);
-} else {
-    console.log('На данный момент покупка дома невозможна.');
-    console.log(`Для покупки дома не хватает ${priceHouse-itog}$`);
-}
+itog>=priceHouse?
+console.log(`Сможет ли Вася купить дом за ${priceHouse}$: ${true}\nОстаток после покупки дома составляет ${itog-priceHouse}$`)
+:
+console.log(`На данный момент покупка дома невозможна. \n Для покупки дома не хватает ${priceHouse-itog}$`);
+// |
