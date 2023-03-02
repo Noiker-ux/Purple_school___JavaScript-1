e0bc87c1c41f6895021bb4238c0f7c87fd1648c0
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 14:52:50 +0200

    Тренировка Стрелочная функция

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-let balance = 2000;
-let bonusBalance = 100;
-let isBanned = false;
-let isExist = true;
-let isSelling = true;
-
-if ((balance>1000 || bonusBalance>100) && (isBanned==false) && (isExist==true) && (isSelling==true)) {
-    console.log('Вы можете купить игру');
-} else {
-    console.log('Вы не можете купить игру');
+const funcTrain = (num, power) => {
+    const res =num**power; 
+    return res;
 }
-    
+console.log(funcTrain(2,3));
