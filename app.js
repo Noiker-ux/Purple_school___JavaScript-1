258d0c16ec48f6f76baab440d1562054ffb46fc4
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 09:43:45 +0200

    Шаблонные строки

--- app.js
+++ app.js
@@ -2,12 +2,10 @@
 const stavkaHour = 80;
 let workTime = dlsworkTime = 40;
 let days = 11-2;
-let payDay = undefined;
 for (i=0;i<days;i++){
     dlsworkTime-=5;
 }
 if (dlsworkTime<=0){
-    console.log('Смогу ли я взять работу? ' + true);
-    payDay=workTime*stavkaHour;
-    console.log('Стоимость работ: ' + payDay + '$');
+    console.log(`Смогу ли я взять работу? ${true}`);
+    console.log(`Стоимость работ: ${workTime*stavkaHour}$`);
 }
