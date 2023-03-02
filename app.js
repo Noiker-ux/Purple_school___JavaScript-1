4e0d37f1cec62fc50f376a38449815fde158e8c9
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-02 11:12:13 +0200

    Задача Проверка на робота (Вариант 2) Тернарый оператор

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const answer = prompt('Сколько будет 7 + или - 15?');
-if (answer==-8 || answer==22) {
-    console.log('Успех');
-} else if ('Я не робот' === answer){
-    console.log('Успех');
-} else {
-    console.log('Вы робот');
-}
+answer==-8 || answer==22?console.log('Успех') : 'Я не робот' === answer ? console.log('Успех') : console.log('Вы робот');
+
+
+    
