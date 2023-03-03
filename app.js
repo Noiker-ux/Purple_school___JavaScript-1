23538c1ba61b31a16d3d3748dbb2cfaf747f9d10
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-03 11:59:12 +0200

    Модернизация прошлой задачи, улучшение проверки + Метод DRY

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const tasks = ['Задача 1','Задача 2','Задача 3'];
+console.log (tasks);
+let haveTask;
 
 const addNewTask = (newTask) =>{
     tasks.push(newTask);
-    console.log (tasks);
     return tasks;
 };
 
+function Helper (HelpTask) {
+    haveTask = undefined;
+    let indxTask = tasks.indexOf(HelpTask);
+    if (indxTask == -1){
+        return tasks;
+    }
+    haveTask = tasks.splice(indxTask,1); 
+    return tasks;
+}
+
 const deleteTask = (deleteTask) =>{
-    tasks.splice(tasks.indexOf(deleteTask),1);
-    console.log(tasks);
+    Helper(deleteTask);
     return tasks;
 };
 
 const warningTask = (warnTask) => {
-    let haveTask = tasks.splice(tasks.indexOf(warnTask),1);
-    tasks.unshift(haveTask[0]);
-    console.log(tasks);
-    return tasks;
+    Helper(warnTask);
+    if (haveTask!=undefined){
+        tasks.unshift(haveTask[0]);
+        return tasks;
+    } else {
+        return tasks;
+    }
 };
 
 let newTask = prompt('Добавить новую задачу');
 addNewTask(newTask);
+console.log (tasks);
 let dropTask = prompt('Удалить задачу');
 deleteTask(dropTask);
+console.log (tasks);
 let warnTask = prompt('Сделать пометку важеное');
 warningTask(warnTask);
+console.log (tasks);
