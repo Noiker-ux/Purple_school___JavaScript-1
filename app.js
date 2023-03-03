c24b32660252c4e27f67176df87f9aabbc64e5c7
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-03 12:29:31 +0200

    Функция разбора URL

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
-const tasks = ['Задача 1','Задача 2','Задача 3'];
-console.log (tasks);
-let haveTask;
+const urlAdress = 'https://purpleschool.ru/course/javascript';
 
-const addNewTask = (newTask) =>{
-    tasks.push(newTask);
-    return tasks;
+function destructurisationUrl(urlAdress) {
+    const destructurisationUrlArray = urlAdress.split('/');
+    console.log(`Доменное имя: ${destructurisationUrlArray}`);
+    let [protocol , _ , adress,...other] = destructurisationUrlArray;
+    console.log(`Доменное имя: ${protocol.slice(0,-1)}\nДоменное имя: ${adress}\nПуть: ${'/'+other.join('/')}`);
+    return destructurisationUrlArray;
 };
-
-function Helper (HelpTask) {
-    haveTask = undefined;
-    let indxTask = tasks.indexOf(HelpTask);
-    if (indxTask == -1){
-        return tasks;
-    }
-    haveTask = tasks.splice(indxTask,1); 
-    return tasks;
-}
-
-const deleteTask = (deleteTask) =>{
-    Helper(deleteTask);
-    return tasks;
-};
-
-const warningTask = (warnTask) => {
-    Helper(warnTask);
-    if (haveTask!=undefined){
-        tasks.unshift(haveTask[0]);
-        return tasks;
-    } else {
-        return tasks;
-    }
-};
-
-let newTask = prompt('Добавить новую задачу');
-addNewTask(newTask);
-console.log (tasks);
-let dropTask = prompt('Удалить задачу');
-deleteTask(dropTask);
-console.log (tasks);
-let warnTask = prompt('Сделать пометку важеное');
-warningTask(warnTask);
-console.log (tasks);
+destructurisationUrl(urlAdress);
