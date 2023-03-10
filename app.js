5c5e52ad13038e66825567a3c3d091dbc2e59350
Вьюгин Александр Сергеевич <a.vyugin@gov39.local>
2023-03-10 10:41:42 +0200

    Проверка телефонов

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-const operation = [1000, -700, 300, -10000, 500];
-const startBalance = 500;
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = ' +7(910) 323-53-56 ';
+/* не верные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
 
-const itogBalance = (startBalance, operation) => {
-    let finallBalance = startBalance;
-    for (let element of operation){
-        finallBalance+=element;
-    }
-    console.log(finallBalance);
-    return finallBalance;
-};
 
-const minusBalance = (startBalance, operation) =>{
-let finallBalance = startBalance;
-    for (let i=0;i<operation.length;i++){
-        if (finallBalance<0){
-            break;
-        } else {
-            finallBalance+=operation[i];
-        }
+const trueNumber = (telNumber) => {
+    let helper, helper1;
+    telNumber = telNumber.trim();
+    telNumber = telNumber.replaceAll('(','');
+    telNumber = telNumber.replaceAll(')','');
+    telNumber = telNumber.replaceAll('-','');
+    telNumber = telNumber.replaceAll(' ','');
+
+    if (telNumber.startsWith('+7')){
+        telNumber = telNumber.replaceAll('+7','8');
     };
-    if (finallBalance<0){
-        console.log(false);
-        return false;
-    } else {
-        return finallBalance;
-    }
-}
 
-const KPD = (operation) =>{
-    let plusBalanses = minusBalances = i = j = 0;
-    let sredPokazatel = [];
-    for (let element of operation){
-        if(element>=0){
-            plusBalanses+=element;
-            i++;
-        } else {
-            minusBalances+=element;
-            j++;
-        }
+    if((telNumber.length==11) && (telNumber.startsWith('8')) && !(isNaN(telNumber))){
+        return true;
+    }else{
+        return false;
     }
-    sredPokazatel.push(plusBalanses/i);
-    sredPokazatel.push(minusBalances/j);
-    console.log(`Средний доход ${sredPokazatel[0]}$`);
-    console.log(`Средний расход ${sredPokazatel[1]}$`);
-    return sredPokazatel;
+
+
+    
 };
 
-itogBalance(startBalance, operation);
-minusBalance(startBalance, operation);
-KPD(operation);
+console.log(trueNumber(num1));
+console.log(trueNumber(num2));
+console.log(trueNumber(num3));
+console.log(trueNumber(num4));
+console.log(trueNumber(num5));
+
+console.log('=====================')
+// //
+console.log(trueNumber(num1Error));
+console.log(trueNumber(num2Error));
+console.log(trueNumber(num3Error));
+console.log(trueNumber(num4Error));
